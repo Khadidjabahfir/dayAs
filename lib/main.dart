e121a0a7f7631bc49522b13fca2678456e84f22a
khadidjabahfir <bahfirkhadidja04@gmail.com>
2025-09-08 12:15:29 -0700

    explored further in the hero tasks widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:dayas/styles/colors.dart';
+import 'package:dayas/widget/customizedUserTask.dart';
 import 'package:dayas/widget/herocard.dart';
-import 'package:dayas/widget/highlighted_subtitle.dart';
-import 'package:dayas/widget/input.dart';
-import 'package:dayas/widget/subtile.dart';
+import 'package:dayas/widget/standardHerotask.dart';
+//import 'package:dayas/widget/highlighted_subtitle.dart';
+import 'package:dayas/widget/standardInput.dart';
+import 'package:dayas/widget/standardSubtitle.dart';
+import 'package:dayas/widget/standardTimeline.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -45,7 +48,11 @@ class _MyHomePageState extends State<MyHomePage> {
           GetInput("Email" , "Enter your email"),
           Subtitle ("I accept the terms and privacy policy"), 
           // Hightlighted_Subtitle("Log_in" , Page)
-          GetHeroCard ("olivia redrigo" , "singer" , "assets/images/test_hero.png" , AppColors.purplePastel)
+          GetHeroCard ("olivia redrigo" , "singer" , "assets/images/test_hero.png" , AppColors.purplePastel),
+          getTimeline(AppColors.purplePastel , 100), 
+          getHeroTask("wake up" , "8:30am" , "" , false  , AppColors.purplePastel), 
+          SizedBox(height: 20,),
+          getUserTask("wake up" , "8:30am" , "" , false , AppColors.purplePastel)
         ],
       )
      
---
+++ lib/widget/customizedUserTask.dart
@@ -0,0 +1,34 @@
+import 'package:dayas/styles/colors.dart';
+import 'package:dayas/styles/lineStyles.dart';
+import 'package:flutter/material.dart';
+
+Widget getUserTask(
+    String taskname, String start, String end, bool isDone, Color color) {
+  return Container(
+    width: 300,
+    height: 40,
+    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+    decoration: BoxDecoration(
+      color: (isDone) ? color : Colors.white,
+      border: Border.all(
+        color: color,
+        width: 1.5,
+      ),
+      borderRadius: BorderRadius.circular(10), 
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          taskname,
+          style: LineStyles.context.copyWith(color: AppColors.lightGrey),
+        ),
+        Text(
+          (end != "") ? "$start - $end" : start,
+          style: LineStyles.context.copyWith(color: AppColors.lightGrey),
+        ),
+      ],
+    ),
+  );
+}
